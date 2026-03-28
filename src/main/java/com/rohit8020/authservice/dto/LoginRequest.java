ed72cefca1dab34b912fc776b77bfa09d3b40205
Rohit Gangwar <rohit8020@Rohits-MacBook-Air.local>
2026-03-28 21:14:14 +0530

    Complete 1

---
+++ src/main/java/com/rohit8020/authservice/dto/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.rohit8020.authservice.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/rohit8020/authservice/entity/User.java
@@ -0,0 +1,24 @@
+package com.rohit8020.authservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    // Example: ROLE_USER, ROLE_ADMIN
+    @Column(nullable = false)
+    private String role;
+}
